b89a301c10642d63e3f5ac566a63b9b5e60d237d
Lingxuan Ye <love@lingxuan.io>
2024-07-05 20:27:54 +0800

    test: rewrite unit test for mod `conversion`

--- src/matrix/conversion.rs
+++ src/matrix/conversion.rs
@@ -119,3 +119,111 @@ impl<T: Clone> From<&[T]> for Matrix<T> {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::matrix;
+
+    #[test]
+    fn test_from_2darray() {
+        let order = Order::default();
+        let shape = AxisShape::from_shape_unchecked(Shape::new(2, 3), order);
+        let data = vec![0, 1, 2, 3, 4, 5];
+        let mut expected = Matrix { order, shape, data };
+
+        let array = [[0, 1, 2], [3, 4, 5]];
+        assert_eq!(Matrix::from(array), expected);
+        assert_eq!(Matrix::from(array.to_vec()), expected);
+        assert_eq!(Matrix::from(&array[..]), expected);
+        assert_eq!(matrix![[0, 1, 2], [3, 4, 5]], expected);
+
+        let array = [[0, 3], [1, 4], [2, 5]];
+        assert_ne!(Matrix::from(array), expected);
+        assert_ne!(Matrix::from(array.to_vec()), expected);
+        assert_ne!(Matrix::from(&array[..]), expected);
+        assert_ne!(matrix![[0, 3], [1, 4], [2, 5]], expected);
+        expected.transpose().set_order(order);
+        assert_eq!(Matrix::from(array), expected);
+        assert_eq!(Matrix::from(array.to_vec()), expected);
+        assert_eq!(Matrix::from(&array[..]), expected);
+        assert_eq!(matrix![[0, 3], [1, 4], [2, 5]], expected);
+    }
+
+    #[test]
+    fn test_try_from_array_of_arrays() {
+        const MAX: usize = isize::MAX as usize;
+
+        let expected = matrix![[0, 1, 2], [3, 4, 5]];
+
+        let aoa = [vec![0, 1, 2], vec![3, 4, 5]];
+        assert_eq!(Matrix::try_from(aoa.clone()).unwrap(), expected);
+        assert_eq!(Matrix::try_from(aoa.to_vec()).unwrap(), expected);
+        assert_eq!(Matrix::try_from(&aoa[..]).unwrap(), expected);
+
+        let aoa = [vec![0, 1, 2]];
+        assert_ne!(Matrix::try_from(aoa.clone()).unwrap(), expected);
+        assert_ne!(Matrix::try_from(aoa.to_vec()).unwrap(), expected);
+        assert_ne!(Matrix::try_from(&aoa[..]).unwrap(), expected);
+
+        let aoa = [vec![0, 1, 2], vec![3, 4, 5], vec![6, 7, 8]];
+        assert_ne!(Matrix::try_from(aoa.clone()).unwrap(), expected);
+        assert_ne!(Matrix::try_from(aoa.to_vec()).unwrap(), expected);
+        assert_ne!(Matrix::try_from(&aoa[..]).unwrap(), expected);
+
+        let aoa = [vec![0, 1], vec![2, 3], vec![4, 5]];
+        assert_ne!(Matrix::try_from(aoa.clone()).unwrap(), expected);
+        assert_ne!(Matrix::try_from(aoa.to_vec()).unwrap(), expected);
+        assert_ne!(Matrix::try_from(&aoa[..]).unwrap(), expected);
+
+        let aoa = [vec![(); MAX], vec![(); MAX]];
+        assert!(Matrix::<()>::try_from(aoa.clone()).is_ok());
+        assert!(Matrix::<()>::try_from(aoa.to_vec()).is_ok());
+        assert!(Matrix::<()>::try_from(&aoa[..]).is_ok());
+
+        let aoa = [vec![(); MAX], vec![(); MAX], vec![(); MAX]];
+        assert_eq!(
+            Matrix::<()>::try_from(aoa.clone()),
+            Err(Error::SizeOverflow)
+        );
+        assert_eq!(
+            Matrix::<()>::try_from(aoa.to_vec()),
+            Err(Error::SizeOverflow)
+        );
+        assert_eq!(Matrix::<()>::try_from(&aoa[..]), Err(Error::SizeOverflow));
+
+        // unable to cover (run out of memory)
+        // let aoa = [vec![0u8; MAX], vec![0u8; MAX]];
+        // assert_eq!(Matrix::<u8>::try_from(aoa.clone()), Err(Error::CapacityExceeded));
+        // assert_eq!(Matrix::<u8>::try_from(aoa.to_vec()), Err(Error::CapacityExceeded));
+        // assert_eq!(Matrix::<u8>::try_from(&aoa[..]), Err(Error::CapacityExceeded));
+
+        let aoa = [vec![0, 1, 2], vec![3, 4]];
+        assert_eq!(
+            Matrix::<i32>::try_from(aoa.clone()),
+            Err(Error::LengthInconsistent)
+        );
+        assert_eq!(
+            Matrix::<i32>::try_from(aoa.to_vec()),
+            Err(Error::LengthInconsistent)
+        );
+        assert_eq!(
+            Matrix::<i32>::try_from(&aoa[..]),
+            Err(Error::LengthInconsistent)
+        );
+    }
+
+    #[test]
+    fn test_from_1darray() {
+        let expected = matrix![[0, 1, 2, 3, 4, 5]];
+
+        let array = [0, 1, 2, 3, 4, 5];
+        assert_eq!(Matrix::from(array), expected);
+        assert_eq!(Matrix::from(array.to_vec()), expected);
+        assert_eq!(Matrix::from(&array[..]), expected);
+
+        let array = [0; 6];
+        assert_ne!(Matrix::from(array), expected);
+        assert_ne!(Matrix::from(array.to_vec()), expected);
+        assert_ne!(Matrix::from(&array[..]), expected);
+    }
+}
