317d8e923cc3fa939ba3dba5e8ae9862567df595
Lingxuan Ye <love@lingxuan.io>
2024-07-05 20:03:16 +0800

    feat: implement conversions from different types of 1D and 2D arrays to `Matrix<T>`

--- src/matrix/conversion.rs
+++ src/matrix/conversion.rs
@@ -12,6 +12,16 @@ impl<T, const R: usize, const C: usize> From<[[T; C]; R]> for Matrix<T> {
     }
 }
 
+impl<T: Clone, const C: usize> From<Vec<[T; C]>> for Matrix<T> {
+    fn from(value: Vec<[T; C]>) -> Self {
+        let order = Order::default();
+        let nrows = value.len();
+        let shape = AxisShape::from_shape_unchecked(Shape::new(nrows, C), order);
+        let data = value.into_iter().flatten().collect();
+        Self { order, shape, data }
+    }
+}
+
 impl<T: Clone, const C: usize> From<&[[T; C]]> for Matrix<T> {
     fn from(value: &[[T; C]]) -> Self {
         let order = Order::default();
@@ -19,6 +29,46 @@ impl<T: Clone, const C: usize> From<&[[T; C]]> for Matrix<T> {
         let shape = AxisShape::from_shape_unchecked(Shape::new(nrows, C), order);
         let data = value.iter().flatten().cloned().collect();
         Self { order, shape, data }
+    }
+}
+
+impl<T: Clone, const C: usize> TryFrom<[Vec<T>; C]> for Matrix<T> {
+    type Error = Error;
+
+    fn try_from(value: [Vec<T>; C]) -> Result<Self> {
+        let order = Order::default();
+        let nrows = C;
+        let ncols = value.first().map_or(0, |row| row.len());
+        let shape = AxisShape::try_from_shape(Shape::new(nrows, ncols), order)?;
+        Self::check_size(shape.size())?;
+        let mut data = Vec::with_capacity(shape.size());
+        for row in value {
+            if row.len() != ncols {
+                return Err(Error::LengthInconsistent);
+            }
+            data.extend(row);
+        }
+        Ok(Self { order, shape, data })
+    }
+}
+
+impl<T: Clone> TryFrom<Vec<Vec<T>>> for Matrix<T> {
+    type Error = Error;
+
+    fn try_from(value: Vec<Vec<T>>) -> Result<Self> {
+        let order = Order::default();
+        let nrows = value.len();
+        let ncols = value.first().map_or(0, |row| row.len());
+        let shape = AxisShape::try_from_shape(Shape::new(nrows, ncols), order)?;
+        Self::check_size(shape.size())?;
+        let mut data = Vec::with_capacity(shape.size());
+        for row in value {
+            if row.len() != ncols {
+                return Err(Error::LengthInconsistent);
+            }
+            data.extend(row);
+        }
+        Ok(Self { order, shape, data })
     }
 }
 
@@ -42,6 +92,24 @@ impl<T: Clone> TryFrom<&[Vec<T>]> for Matrix<T> {
     }
 }
 
+impl<T: Clone, const N: usize> From<[T; N]> for Matrix<T> {
+    fn from(value: [T; N]) -> Self {
+        let order = Order::default();
+        let shape = AxisShape::from_shape_unchecked(Shape::new(1, N), order);
+        let data = value.to_vec();
+        Self { order, shape, data }
+    }
+}
+
+impl<T: Clone> From<Vec<T>> for Matrix<T> {
+    fn from(value: Vec<T>) -> Self {
+        let order = Order::default();
+        let shape = AxisShape::from_shape_unchecked(Shape::new(1, value.len()), order);
+        let data = value;
+        Self { order, shape, data }
+    }
+}
+
 impl<T: Clone> From<&[T]> for Matrix<T> {
     fn from(value: &[T]) -> Self {
         let order = Order::default();
@@ -51,76 +119,3 @@ impl<T: Clone> From<&[T]> for Matrix<T> {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::matrix;
-
-    #[test]
-    fn test_from_2darray() {
-        let order = Order::default();
-        let shape = AxisShape::from_shape_unchecked(Shape::new(2, 3), order);
-        let data = vec![0, 1, 2, 3, 4, 5];
-        let expected = Matrix { order, shape, data };
-
-        let array = [[0, 1, 2], [3, 4, 5]];
-        assert_eq!(Matrix::from(array), expected);
-        assert_eq!(matrix![[0, 1, 2], [3, 4, 5]], expected);
-
-        let array = [[0, 1], [2, 3], [4, 5]];
-        assert_ne!(Matrix::from(array), expected);
-        assert_ne!(matrix![[0, 1], [2, 3], [4, 5]], expected);
-    }
-
-    #[test]
-    fn test_from_slice_of_arrays() {
-        let expected = matrix![[0, 1, 2], [3, 4, 5]];
-
-        let arrays = [[0, 1, 2], [3, 4, 5]];
-        assert_eq!(Matrix::from(&arrays[..]), expected);
-
-        let arrays = [[0, 1, 2]];
-        assert_ne!(Matrix::from(&arrays[..]), expected);
-
-        let arrays = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
-        assert_ne!(Matrix::from(&arrays[..]), expected);
-
-        let arrays = [[0, 1], [2, 3], [4, 5]];
-        assert_ne!(Matrix::from(&arrays[..]), expected);
-    }
-
-    #[test]
-    fn test_try_from_slice_of_vecs() {
-        const MAX: usize = isize::MAX as usize;
-
-        let expected = matrix![[0, 1, 2], [3, 4, 5]];
-
-        let vecs = [vec![0, 1, 2], vec![3, 4, 5]];
-        assert_eq!(Matrix::try_from(&vecs[..]).unwrap(), expected);
-
-        let vecs = [vec![0, 1, 2]];
-        assert_ne!(Matrix::try_from(&vecs[..]).unwrap(), expected);
-
-        let vecs = [vec![0, 1, 2], vec![3, 4, 5], vec![6, 7, 8]];
-        assert_ne!(Matrix::try_from(&vecs[..]).unwrap(), expected);
-
-        let vecs = [vec![0, 1], vec![2, 3], vec![4, 5]];
-        assert_ne!(Matrix::try_from(&vecs[..]).unwrap(), expected);
-
-        let vecs = [vec![(); MAX], vec![(); MAX]];
-        assert!(Matrix::<()>::try_from(&vecs[..]).is_ok());
-
-        let vecs = [vec![(); MAX], vec![(); MAX], vec![(); MAX]];
-        assert_eq!(Matrix::<()>::try_from(&vecs[..]), Err(Error::SizeOverflow));
-
-        // unable to cover (run out of memory)
-        // let vecs = [vec![0u8; MAX], vec![0u8; MAX]];
-        // assert_eq!(Matrix::<u8>::try_from(&vecs[..]), Err(Error::CapacityExceeded));
-
-        let vecs = [vec![0, 1, 2], vec![3, 4]];
-        assert_eq!(
-            Matrix::<i32>::try_from(&vecs[..]),
-            Err(Error::LengthInconsistent)
-        );
-    }
-}
