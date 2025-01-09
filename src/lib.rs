5b2b209888249c2ad08310430bda2db4ac620a30
Lingxuan Ye <love@lingxuan.io>
2025-01-10 07:21:50 +0800

    docs: add more examples in Quick Start

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,18 @@
 //! use matreex::matrix;
 //!
 //! let lhs = matrix![[0, 1, 2], [3, 4, 5]];
+//! let rhs = matrix![[2, 2, 2], [2, 2, 2]];
+//! assert_eq!(lhs + rhs, matrix![[2, 3, 4], [5, 6, 7]]);
+//!
+//! let lhs = matrix![[0, 1, 2], [3, 4, 5]];
 //! let rhs = matrix![[0, 1], [2, 3], [4, 5]];
 //! assert_eq!(lhs * rhs, matrix![[10, 13], [28, 40]]);
+//!
+//! let matrix = matrix![[0, 1, 2], [3, 4, 5]];
+//! assert_eq!(matrix - 2, matrix![[-2, -1, 0], [1, 2, 3]]);
+//!
+//! let matrix = matrix![[0, 1, 2], [3, 4, 5]];
+//! assert_eq!(2 - matrix, matrix![[2, 1, 0], [-1, -2, -3]]);
 //! ```
 //!
 //! # FAQs
