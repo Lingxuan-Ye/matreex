5ec946710572711901bb13c2ab961be92dd48238
Lingxuan Ye <love@lingxuan.io>
2025-01-22 17:41:53 +0800

    refactor: improve readability

--- src/matrix/fmt.rs
+++ src/matrix/fmt.rs
@@ -1,6 +1,7 @@
 use super::index::Index;
 use super::Matrix;
 use std::collections::VecDeque;
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
 
 const LEFT_DELIMITER: &str = "[";
 const RIGHT_DELIMITER: &str = "]";
@@ -24,8 +25,11 @@ macro_rules! write_index {
     };
 }
 
-impl<T: std::fmt::Debug> std::fmt::Debug for Matrix<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+impl<T> Debug for Matrix<T>
+where
+    T: Debug,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
         let shape = self.shape();
         let nrows = shape.nrows();
         let ncols = shape.ncols();
@@ -112,8 +116,11 @@ impl<T: std::fmt::Debug> std::fmt::Debug for Matrix<T> {
     }
 }
 
-impl<T: std::fmt::Display> std::fmt::Display for Matrix<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+impl<T> Display for Matrix<T>
+where
+    T: Display,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
         if self.is_empty() {
             return write!(f, "{LEFT_DELIMITER}{RIGHT_DELIMITER}");
         }
@@ -180,11 +187,17 @@ impl<T: std::fmt::Display> std::fmt::Display for Matrix<T> {
 struct Lines(VecDeque<String>);
 
 impl Lines {
-    fn from_debug<T: std::fmt::Debug>(element: T) -> Self {
+    fn from_debug<T>(element: T) -> Self
+    where
+        T: Debug,
+    {
         Self(format!("{:?}", element).lines().map(String::from).collect())
     }
 
-    fn from_display<T: std::fmt::Display>(element: T) -> Self {
+    fn from_display<T>(element: T) -> Self
+    where
+        T: Display,
+    {
         Self(format!("{}", element).lines().map(String::from).collect())
     }
 
