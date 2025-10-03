7b49a1533e5cdd09f5fbcd1471cbbff8ff2d6a95
Lingxuan Ye <love@lingxuan.io>
2025-10-03 14:27:52 +0800

    feat: support `PartialEq` between matrices of different element types and orders

--- src/dense.rs
+++ src/dense.rs
@@ -257,13 +257,28 @@ where
     }
 }
 
-impl<T, O> PartialEq for Matrix<T, O>
+impl<L, R, LO, RO> PartialEq<Matrix<R, RO>> for Matrix<L, LO>
 where
-    T: PartialEq,
-    O: Order,
+    L: PartialEq<R>,
+    LO: Order,
+    RO: Order,
 {
-    fn eq(&self, other: &Self) -> bool {
-        self.layout == other.layout && self.data == other.data
+    fn eq(&self, other: &Matrix<R, RO>) -> bool {
+        if self.shape() != other.shape() {
+            false
+        } else if LO::KIND == RO::KIND {
+            self.data == other.data
+        } else {
+            let left_stride = self.stride();
+            let right_stride = other.stride();
+            self.data.iter().enumerate().all(|(index, left)| {
+                let index = LayoutIndex::from_flattened(index, left_stride)
+                    .swap()
+                    .to_flattened(right_stride);
+                let right = unsafe { other.data.get_unchecked(index) };
+                left == right
+            })
+        }
     }
 }
 
