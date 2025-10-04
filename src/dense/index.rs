c252a2e41755d2deaac309c3dda8d481173ba60e
Lingxuan Ye <love@lingxuan.io>
2025-10-04 18:08:29 +0800

    feat: define and implement trait `MatrixIndex`

--- src/dense/index.rs
+++ src/dense/index.rs
@@ -1,7 +1,180 @@
+use super::Matrix;
 use super::layout::{Order, OrderKind, Stride};
-use crate::index::Index;
+use crate::error::Result;
+use crate::index::{AsIndex, Index, MatrixIndex, WrappingIndex};
+
+impl<T, O> Matrix<T, O>
+where
+    O: Order,
+{
+    /// Returns a reference to the [`MatrixIndex::Output`]
+    /// at given location.
+    ///
+    /// # Errors
+    ///
+    /// - [`Error::IndexOutOfBounds`] if out of bounds.
+    pub fn get<I>(&self, index: I) -> Result<&I::Output>
+    where
+        I: MatrixIndex<Self>,
+    {
+        index.get(self)
+    }
+
+    /// Returns a mutable reference to the [`MatrixIndex::Output`]
+    /// at given location.
+    ///
+    /// # Errors
+    ///
+    /// - [`Error::IndexOutOfBounds`] if out of bounds.
+    pub fn get_mut<I>(&mut self, index: I) -> Result<&mut I::Output>
+    where
+        I: MatrixIndex<Self>,
+    {
+        index.get_mut(self)
+    }
+
+    /// Returns a reference to the [`MatrixIndex::Output`]
+    /// at given location, without performing any bounds checking.
+    ///
+    /// # Safety
+    ///
+    /// Calling this method with an out-of-bounds index is *[undefined
+    /// behavior]* even if the resulting reference is not used.
+    ///
+    /// For a safe alternative see [`get`].
+    ///
+    /// [undefined behavior]: https://doc.rust-lang.org/reference/behavior-considered-undefined.html
+    /// [`get`]: Matrix::get
+    pub unsafe fn get_unchecked<I>(&self, index: I) -> &I::Output
+    where
+        I: MatrixIndex<Self>,
+    {
+        unsafe { index.get_unchecked(self) }
+    }
+
+    /// Returns a mutable reference to the [`MatrixIndex::Output`]
+    /// at given location, without performing any bounds checking.
+    ///
+    /// # Safety
+    ///
+    /// Calling this method with an out-of-bounds index is *[undefined
+    /// behavior]* even if the resulting reference is not used.
+    ///
+    /// For a safe alternative see [`get_mut`].
+    ///
+    /// [undefined behavior]: https://doc.rust-lang.org/reference/behavior-considered-undefined.html
+    /// [`get_mut`]: Matrix::get_mut
+    pub unsafe fn get_unchecked_mut<I>(&mut self, index: I) -> &mut I::Output
+    where
+        I: MatrixIndex<Self>,
+    {
+        unsafe { index.get_unchecked_mut(self) }
+    }
+}
+
+impl<T, O, I> core::ops::Index<I> for Matrix<T, O>
+where
+    O: Order,
+    I: MatrixIndex<Self>,
+{
+    type Output = I::Output;
+
+    fn index(&self, index: I) -> &Self::Output {
+        index.index(self)
+    }
+}
+
+impl<T, O, I> core::ops::IndexMut<I> for Matrix<T, O>
+where
+    O: Order,
+    I: MatrixIndex<Self>,
+{
+    fn index_mut(&mut self, index: I) -> &mut Self::Output {
+        index.index_mut(self)
+    }
+}
+
+unsafe impl<T, O, I> MatrixIndex<Matrix<T, O>> for I
+where
+    O: Order,
+    I: AsIndex,
+{
+    type Output = T;
+
+    fn is_out_of_bounds(&self, matrix: &Matrix<T, O>) -> bool {
+        let shape = matrix.shape();
+        self.row() >= shape.nrows() || self.col() >= shape.ncols()
+    }
+
+    unsafe fn get_unchecked(self, matrix: &Matrix<T, O>) -> &Self::Output {
+        let stride = matrix.stride();
+        let index = Index::from_as_index(self).to_flattened::<O>(stride);
+        unsafe { matrix.data.get_unchecked(index) }
+    }
+
+    unsafe fn get_unchecked_mut(self, matrix: &mut Matrix<T, O>) -> &mut Self::Output {
+        let stride = matrix.stride();
+        let index = Index::from_as_index(self).to_flattened::<O>(stride);
+        unsafe { matrix.data.get_unchecked_mut(index) }
+    }
+}
+
+unsafe impl<T, O> MatrixIndex<Matrix<T, O>> for WrappingIndex
+where
+    O: Order,
+{
+    type Output = T;
+
+    /// Returns `true` if the index is out of bounds for the given matrix.
+    ///
+    /// # Notes
+    ///
+    /// A wrapping index is out of bounds if and only if the matrix is empty.
+    fn is_out_of_bounds(&self, matrix: &Matrix<T, O>) -> bool {
+        matrix.is_empty()
+    }
+
+    /// Returns a shared reference to the output at this location,
+    /// without performing any bounds checking.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the matrix is empty.
+    ///
+    /// # Safety
+    ///
+    /// This method is safe, despite being marked `unsafe`. If no panic
+    /// occurs, the output returned is guaranteed to be valid.
+    unsafe fn get_unchecked(self, matrix: &Matrix<T, O>) -> &Self::Output {
+        let shape = matrix.shape();
+        let stride = matrix.stride();
+        let index = Index::from_wrapping_index(self, shape).to_flattened::<O>(stride);
+        unsafe { matrix.data.get_unchecked(index) }
+    }
+
+    /// Returns a mutable reference to the output at this location,
+    /// without performing any bounds checking.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the matrix is empty.
+    ///
+    /// # Safety
+    ///
+    /// This method is safe, despite being marked `unsafe`. If no panic
+    /// occurs, the output returned is guaranteed to be valid.
+    unsafe fn get_unchecked_mut(self, matrix: &mut Matrix<T, O>) -> &mut Self::Output {
+        let shape = matrix.shape();
+        let stride = matrix.stride();
+        let index = Index::from_wrapping_index(self, shape).to_flattened::<O>(stride);
+        unsafe { matrix.data.get_unchecked_mut(index) }
+    }
+}
 
 impl Index {
+    /// # Panics
+    ///
+    /// Panics if `stride.major() == 0`.
     pub(super) fn from_flattened<O>(index: usize, stride: Stride) -> Self
     where
         O: Order,
--- src/index.rs
+++ src/index.rs
@@ -1,4 +1,115 @@
+use crate::error::{Error, Result};
 use crate::shape::AsShape;
+
+/// A helper trait for indexing operations on a matrix.
+///
+/// # Safety
+///
+/// Implementations of this trait have to promise that if any default
+/// implementation of [`get`], [`get_mut`], [`index`] or [`index_mut`]
+/// is used, then [`is_out_of_bounds`] is implemented correctly and
+/// [`ensure_in_bounds`] is not overridden. Failing to do so may result
+/// in an out-of-bounds memory access, leading to *[undefined behavior]*.
+///
+/// [`is_out_of_bounds`]: MatrixIndex::is_out_of_bounds
+/// [`ensure_in_bounds`]: MatrixIndex::ensure_in_bounds
+/// [`get`]: MatrixIndex::get
+/// [`get_mut`]: MatrixIndex::get_mut
+/// [`index`]: MatrixIndex::index
+/// [`index_mut`]: MatrixIndex::index_mut
+/// [undefined behavior]: https://doc.rust-lang.org/reference/behavior-considered-undefined.html
+pub unsafe trait MatrixIndex<M>: Sized {
+    /// The output type returned by methods.
+    type Output;
+
+    /// Returns `true` if the index is out of bounds for the given matrix.
+    fn is_out_of_bounds(&self, matrix: &M) -> bool;
+
+    /// Ensures the index is in bounds for the given matrix.
+    ///
+    /// # Errors
+    ///
+    /// - [`Error::IndexOutOfBounds`] if out of bounds.
+    fn ensure_in_bounds(&self, matrix: &M) -> Result<&Self> {
+        if self.is_out_of_bounds(matrix) {
+            Err(Error::IndexOutOfBounds)
+        } else {
+            Ok(self)
+        }
+    }
+
+    /// Returns a shared reference to the output at this location,
+    /// if in bounds.
+    ///
+    /// # Errors
+    ///
+    /// - [`Error::IndexOutOfBounds`] if out of bounds.
+    ///
+    /// [`Output`]: MatrixIndex::Output
+    fn get(self, matrix: &M) -> Result<&Self::Output> {
+        self.ensure_in_bounds(matrix)?;
+        unsafe { Ok(self.get_unchecked(matrix)) }
+    }
+
+    /// Returns a mutable reference to the output at this location,
+    /// if in bounds.
+    ///
+    /// # Errors
+    ///
+    /// - [`Error::IndexOutOfBounds`] if out of bounds.
+    ///
+    /// [`OutputMut`]: MatrixIndex::OutputMut
+    fn get_mut(self, matrix: &mut M) -> Result<&mut Self::Output> {
+        self.ensure_in_bounds(matrix)?;
+        unsafe { Ok(self.get_unchecked_mut(matrix)) }
+    }
+
+    /// Returns a shared reference to the output at this location,
+    /// without performing any bounds checking.
+    ///
+    /// # Safety
+    ///
+    /// Calling this method with an out-of-bounds index is *[undefined
+    /// behavior]* even if the resulting reference is not used.
+    ///
+    /// [undefined behavior]: https://doc.rust-lang.org/reference/behavior-considered-undefined.html
+    unsafe fn get_unchecked(self, matrix: &M) -> &Self::Output;
+
+    /// Returns a mutable reference to the output at this location,
+    /// without performing any bounds checking.
+    ///
+    /// # Safety
+    ///
+    /// Calling this method with an out-of-bounds index is *[undefined
+    /// behavior]* even if the resulting reference is not used.
+    ///
+    /// [undefined behavior]: https://doc.rust-lang.org/reference/behavior-considered-undefined.html
+    unsafe fn get_unchecked_mut(self, matrix: &mut M) -> &mut Self::Output;
+
+    /// Returns a shared reference to the output at this location.
+    ///
+    /// # Panics
+    ///
+    /// Panics if out of bounds.
+    fn index(self, matrix: &M) -> &Self::Output {
+        match self.get(matrix) {
+            Err(error) => panic!("{error}"),
+            Ok(output) => output,
+        }
+    }
+
+    /// Returns a mutable reference to the output at this location.
+    ///
+    /// # Panics
+    ///
+    /// Panics if out of bounds.
+    fn index_mut(self, matrix: &mut M) -> &mut Self::Output {
+        match self.get_mut(matrix) {
+            Err(error) => panic!("{error}"),
+            Ok(output) => output,
+        }
+    }
+}
 
 #[derive(Clone, Copy, Debug, Default, Hash, PartialEq, Eq)]
 pub struct Index {
@@ -9,6 +120,15 @@ pub struct Index {
 impl Index {
     #[inline]
     pub fn new(row: usize, col: usize) -> Self {
+        Self { row, col }
+    }
+
+    pub fn from_as_index<I>(index: I) -> Self
+    where
+        I: AsIndex,
+    {
+        let row = index.row();
+        let col = index.col();
         Self { row, col }
     }
 
@@ -24,10 +144,9 @@ impl Index {
             }
         }
 
-        Self {
-            row: rem_euclid(index.row, shape.nrows()),
-            col: rem_euclid(index.col, shape.ncols()),
-        }
+        let row = rem_euclid(index.row, shape.nrows());
+        let col = rem_euclid(index.col, shape.ncols());
+        Self { row, col }
     }
 
     #[inline]
